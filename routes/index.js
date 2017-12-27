db6864ceb5eaf1d4a2ab6f1ef1f73c646d9785d8
Carlos <carlos1337@gmail.com>
2017-12-26 21:46:02 -0500

    Changed the method used to log connections

--- routes/index.js
+++ routes/index.js
@@ -7,63 +7,44 @@ AWS.config.apiVersions = {dynamodb: 'latest'}
 const uuidv1 = require('uuid/v1');
 const requestIp = require('request-ip');
 
-router.get('/ips', function(req,res,next){
+router.get('/get/:id', function(req,res,next){
+  var id = req.params.id;
   var docClient = new AWS.DynamoDB.DocumentClient();
 
   var params = {
-      TableName: "connections",
-      ProjectionExpression: "ip",
+      TableName: "clients",
+      Key: {"uuid": id}
   };
 
-  console.log("Scanning connections table.");
-  docClient.scan(params, onScan);
-
-  function onScan(err, data) {
-      if (err) {
-          console.error("Unable to scan the table. Error JSON:", JSON.stringify(err, null, 2));
-      } else {
-          // print all the movies
-          console.log("Scan succeeded.");
-          console.log(data.Items.map(x => x.ip));
-          res.render('ip_list',{ips: data.Items.map(x => x.ip)});
-      }
-  }
-});
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  /*require('dns').reverse(req.header('x-forwarded-for'), function(err, domains) {
-    if(err) {
-        console.log(err.toString());
-        return;
-    }
-    console.log(domains);
-});*/
-  var docClient = new AWS.DynamoDB.DocumentClient();
-  var table = "connections";
-
-
-  console.log(req);
-  require('dns').reverse(req.connection.remoteAddress, function(err, domains) {
-    res.render('index', { title: requestIp.getClientIp(req) });
-      console.log(domains);
-      var params = {
-          TableName:table,
-          Item:{
-              "id": uuidv1(),
-              "ip": requestIp.getClientIp(req),
-              "name": domains
+  docClient.get(params,function(err,data){
+      if(err){
+          res.render('index', { title: err });
+      }else if(data){
+        console.log(data);
+        var dateFormat = require('dateformat');
+        var putParams = {
+            TableName:"connections",
+            Item:{
+                "id": uuidv1(),
+                "login": data.Item.login,
+                "date": dateFormat(new Date(), "yyyy-mm-dd HH:MM:ss")
+            }
+        };
+        docClient.put(putParams, function(err, data) {
+          if (err) {
+            console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+          } else {
+            console.log("Added item:):", JSON.stringify(data, null, 2));
           }
-      };
-      docClient.put(params, function(err, data) {
-        if (err) {
-          console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-        } else {
-          console.log("Added item:):", JSON.stringify(data, null, 2));
-        }
-      });
+        });
+        res.render('index', { title: "Ok!"});
+      }else{
+        res.render('index', { title: "No results" });
+      }
   });
 
+
 });
+
 
 module.exports = router;
